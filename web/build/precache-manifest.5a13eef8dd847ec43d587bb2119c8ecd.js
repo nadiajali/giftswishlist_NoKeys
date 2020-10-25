f30d2c95a0162d590e5f7bee6bd93f7d7091099a
nadiajali <nadiajali@hotmail.com>
2020-10-25 14:45:58 -0400

    Changed gitignore

---
+++ web/build/precache-manifest.5a13eef8dd847ec43d587bb2119c8ecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f13da80762bc70e07749af31191cd61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbe989edc3637c837ee3",
+    "url": "/static/css/main.d42b83ac.chunk.css"
+  },
+  {
+    "revision": "21c3c08e7fee43e5e50c",
+    "url": "/static/js/2.d4926984.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4926984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe989edc3637c837ee3",
+    "url": "/static/js/main.88f8dfa8.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "46b032cec2b575ad973463abec64a11c",
+    "url": "/static/media/accountinggifts.46b032ce.jpg"
+  }
+]);
